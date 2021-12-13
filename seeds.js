152c742f7f4b37ef80093a9302f1816fadf397ee
Louis Fletcher <Louisco1999@gmail.com>
2021-12-13 10:10:37 +0000

    added countries

---
+++ seeds.js
@@ -0,0 +1,367 @@
+[
+     
+    {
+        "name": "United Kingdom",
+        "officialName": "United Kingdom of Great Britain and Northern Ireland",
+        "continent": "Europe",
+        "capital": "London",
+        "population": 67215293,
+        "currency": {
+                "officialName": "British pound",
+                "symbol": "£",
+                "name": "GBP"
+                 },
+        "areaKM": 242495,
+        "highestPoint": {
+                        "tallestpointM": 1343,
+                        "location": "Ben Nevis"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 917,
+                  "medals": {"gold": 286, "silver": 316, "bronze": 315}
+                  },
+        "About" : "The United Kingdom, made up of England, Scotland, Wales and Northern Ireland, is an island nation in northwestern Europe",
+        "GDP-dollars": 2708000000000,
+        "officialLanguage": "English",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/gb.svg",
+        "borderingCountries": ["Ireland"],
+        "nationalAnimal": "Lion",
+        "animalPhoto": "https://en.wikipedia.org/wiki/Lion#/media/File:Lion_waiting_in_Namibia.jpg",
+        "googleMap": "https://goo.gl/maps/FoDtc3UKMkFsXAjHA"
+    }, 
+
+    {
+        "name": "France",
+        "officialName": "French Republic",
+        "continent": "Europe",
+        "capital": "Paris",
+        "population": 67391582,
+        "currency": {
+                "officialName": "Euro",
+                "symbol": "€",
+                "name": "Euro"
+                 },
+        "areaKM": 547660 ,
+        "highestPoint": {
+                        "tallestpointM": 4810,
+                        "location": "Mont Blanc"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 761,
+                  "medals": {"gold": 225, "silver": 257, "bronze": 279}
+                  },
+        "About" : "France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine",
+        "GDP-dollars": 2603000000000,
+        "officialLanguage": "",   
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/fr.svg",
+        "borderingCountries": ["Andorra", "Belgium", "Germany", "Italy", "Luxembourg", "Monaco", "Spain", "Switzerland"],
+        "nationalAnimal": "The Gallic Rooster",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:Coq-gaulois-dore.JPG",
+        "googleMap": "https://goo.gl/maps/g7QxxSFsWyTPKuzd7"
+    },
+    {
+        "name": "Spain",
+        "officialName": "Kingdom of Spain",
+        "continent": "Europe",
+        "capital": "Madrid",
+        "population": 47351567,
+        "currency": {
+                "officialName": "Euro",
+                "symbol": "€",
+                "name": "Euro"
+                 },
+        "areaKM": 505992,
+        "highestPoint": {
+                        "tallestpointM": 3479,
+                        "location": "Mulhacen"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 168,
+                  "medals": {"gold": 48, "silver": 78, "bronze": 48}
+                  },
+        "About" : "Spain, a country on Europe’s Iberian Peninsula, includes 17 autonomous regions with diverse geography and cultures",
+        "officialLanguage": "Spanish",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/es.svg",
+        "borderingCountries": ["Andorra", "France", "Gibraltar", "Portugal", "Morocco"],
+        "nationalAnimal": "Bull",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_animals_representing_first-level_administrative_country_subdivisions#/media/File:Toro_de_Osborne_(Llanes,_Asturias)_01.jpg",
+        "googleMap": "https://goo.gl/maps/138JaXW8EZzRVitY9"
+    },
+    {
+        "name": "Italy",
+        "officialName": "Italian Republic",
+        "continent": "Europe",
+        "capital": "Rome",
+        "population": 59554023,
+        "currency": {
+                "officialName": "Euro",
+                "symbol": "€",
+                "name": "Euro"
+                 },
+        "areaKM": 301336,
+        "highestPoint": {
+                        "tallestpointM": 4810,
+                        "location": "Monte Bianco"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 618,
+                  "medals": {"gold": 217, "silver": 188, "bronze": 213}
+                  },
+        "About" : "Italy, a European country with a long Mediterranean coastline, has left a powerful mark on Western culture and cuisine. Its capital, Rome, is home to the Vatican as well as landmark art and ancient ruins. Other major cities include Florence",
+        "officialLanguage": "Italian",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/it.svg",
+        "borderingCountries": ["Austria", "France", "San Marino", "Slovenia", "Switzerland", "Vatican City"],
+        "nationalAnimal": "Italian Wolf",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:Lupo_appenninico_3.jpg",
+        "googleMap": "https://goo.gl/maps/8M1K27TDj7StTRTq8"
+    },
+    {
+        "name": "Germany",
+        "officialName": "Federal Republic of Germany",
+        "continent": "Europe",
+        "capital": "Berlin",
+        "population": 83240525,
+        "currency": {
+                "officialName": "Euro",
+                "symbol": "€",
+                "name": "Euro"
+                 },
+        "areaKM": 357114,
+        "highestPoint": {
+                        "tallestpointM": 2962,
+                        "location": "The Zugspitze"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 653,
+                  "medals": {"gold": 201, "silver": 206, "bronze": 246}
+                  },
+        "About" : "Germany is a Western European country with a landscape of forests, rivers, mountain ranges and North Sea beaches. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes, the Brandenburg Gate and many sites relating to WWII. Munich is known for its Oktoberfest and beer halls",
+        "officialLanguage": "German",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/de.svg",
+        "borderingCountries": ["Austria", "Belgium", "Czech Republic", "Denmark", "France", "Luxembourg", "Netherlands", "Poland", "Switzerland"],
+        "nationalAnimal": "Federal eagle",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_birds#/media/File:Aquila_chrysaetos_Flickr.jpg",
+        "googleMap": "https://goo.gl/maps/mD9FBMq1nvXUBrkv6"
+    },
+    {
+        "name": "United States",
+        "officialName": "United States of America",
+        "continent": "North America",
+        "capital": "Washington, D.C.",
+        "population": 329484123,
+        "currency": {
+                "officialName": "United States Dollar",
+                "symbol": "$",
+                "name": "USD"
+                 },
+        "areaKM": 9372610,
+        "highestPoint": {
+                        "tallestpointM": 6190,
+                        "location": "Denali"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 2632,
+                  "medals": {"gold": 1061, "silver": 832, "bronze": 739}
+                  },
+        "About" : "The U.S. is a country of 50 states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. Major Atlantic Coast cities are New York, a global finance and culture center, and capital Washington, DC. Midwestern metropolis Chicago is known for influential architecture and on the west coast, Los Angeles' Hollywood is famed for filmmaking.",
+        "officialLanguage": "English",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/us.svg",
+        "borderingCountries": ["Canada", "Mexico"],
+        "nationalAnimal": "Bald Eagle",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_birds#/media/File:Bald_eagle_closeup_16x9.jpg",
+        "googleMap": "https://goo.gl/maps/e8M246zY4BSjkjAv6"
+    },
+    {
+        "name": "Mexico",
+        "officialName": "United Mexican States",
+        "continent": "North America",
+        "capital": "Mexico City",
+        "population": 128932753,
+        "currency": {
+                "officialName": "Mexican peso",
+                "symbol": "$",
+                "name": "MXN"
+                 },
+        "areaKM": 1964375,
+        "highestPoint": {
+                        "tallestpointM": 5675,
+                        "location": "Pico de Orizaba"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 73,
+                  "medals": {"gold": 13, "silver": 24, "bronze": 36}
+                  },
+        "About" : "Mexico, officially the United Mexican States, is a country in the southern portion of North America. It is bordered to the north by the United States; to the south and west by the Pacific Ocean; to the southeast by Guatemala, Belize, and the Caribbean Sea; and to the east by the Gulf of Mexico",
+        "officialLanguage": "Spanish",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/mx.svg",
+        "borderingCountries": ["Belize", "Guatemala", "United States"],
+        "nationalAnimal": "Golden Eagle",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_birds#/media/File:GoldenEagle-Nova.jpg",
+        "googleMap": "https://goo.gl/maps/s5g7imNPMDEePxzbA"
+    },
+    {
+        "name": "Canada",
+        "officialName": "Canada",
+        "continent": "North America",
+        "capital": "Ottawa",
+        "population": 38005238,
+        "currency": {
+                "officialName": "Canadian Dollar",
+                "symbol": "$",
+                "name": "CAD"
+                 },
+        "areaKM": 9984670,
+        "highestPoint": {
+                        "tallestpointM": 5959,
+                        "location": "Mount Logan"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 326,
+                  "medals": {"gold": 71, "silver": 108, "bronze": 147}
+                  },
+        "About" : "Canada is a country in North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres, making it the world's second-largest country by total area",
+        "officialLanguage": "French",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/ca.svg",
+        "borderingCountries": ["United States"],
+        "nationalAnimal": "North American beaver",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:American_Beaver.jpg",
+        "googleMap": "https://goo.gl/maps/jmEVLugreeqiZXxbA"
+    },
+    {
+        "name": "Jamaica",
+        "officialName": "Jamaica",
+        "continent": "North America",
+        "capital": "Kingston",
+        "population": 2961161,
+        "currency": {
+                "officialName": "Jamaican dollar",
+                "symbol": "$",
+                "name": "Jamaican dollar"
+                 },
+        "areaKM": 10991,
+        "highestPoint": {
+                        "tallestpointM": 2256,
+                        "location": "Blue Mountain Peak"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 87,
+                  "medals": {"gold": 26, "silver": 36, "bronze": 25}
+                  },
+        "About" : "Jamaica, a Caribbean island nation, has a lush topography of mountains, rainforests and reef-lined beaches. Many of its all-inclusive resorts are clustered in Montego Bay, with its British-colonial architecture, and Negril, known for its diving and snorkeling sites. Jamaica is famed as the birthplace of reggae music, and its capital Kingston is home to the Bob Marley Museum, dedicated to the famous singer.",
+        "officialLanguage": "English",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/jm.svg",
+        "borderingCountries": 0,
+        "nationalAnimal": "Red-Billed streamertail",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:Red-billed_Streamertail_2506104129.jpg",
+        "googleMap": "https://goo.gl/maps/Z8mQ6jxnRQKFwJy9A"
+    },
+    {
+        "name": "Guatemala",
+        "officialName": "Republic of Guatemala",
+        "continent": " North America",
+        "capital": "Guatemala City",
+        "population": 16858333,
+        "currency": {
+                "officialName": "Guatemalan quetzal",
+                "symbol": "Q",
+                "name": "GTQ"
+                 },
+        "areaKM": 108889,
+        "highestPoint": {
+                        "tallestpointM": 4220,
+                        "location": "Volcán Tajumulco"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 1,
+                  "medals": {"gold": 0, "silver": 1, "bronze": 0}
+                  },
+        "About" : "Guatemala, a Central American country south of Mexico, is home to volcanoes, rainforests and ancient Mayan sites. The capital, Guatemala City, features the stately National Palace of Culture and the National Museum of Archaeology and Ethnology. Antigua, west of the capital, contains preserved Spanish colonial buildings. Lake Atitlán, formed in a massive volcanic crater, is surrounded by coffee fields and villages.",
+        "officialLanguage": "Spanish",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/gt.svg",
+        "borderingCountries": ["Belize", "El Salvador", "Honduras", "Mexico"],
+        "nationalAnimal": "Quetzal",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:Quetzal01.jpg",
+        "googleMap": "https://goo.gl/maps/JoRAbem4Hxb9FYbVA"
+    },
+    {
+        "name": "Papua New Guinea",
+        "officialName": "Independent State of Papua New Guinea",
+        "continent": "Oceania",
+        "capital": "Port Moresby",
+        "population": 8947027,
+        "currency": {
+                "officialName": "Papua New Guinean kina",
+                "symbol": "K",
+                "name": "PGK"
+                 },
+        "areaKM": 462840,
+        "highestPoint": {
+                        "tallestpointM": 4509,
+                        "location": "Mount Wilhelm"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 0,
+                  "medals": {"gold": 0, "silver": 0, "bronze": 0}
+                  },
+        "About" : "Southern Region is one of four regions of Papua New Guinea. The region includes the national capital Port Moresby",
+        "officialLanguage": "English",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/pg.svg",
+        "borderingCountries": ["Indonesia"],
+        "nationalAnimal": "Dugong",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:Dugong_Marsa_Alam.jpg",
+        "googleMap": "https://goo.gl/maps/ChGmzZBjZ3vnBwR2A"
+    },
+    {
+        "name": "New Zealand",
+        "officialName": "New Zealand",
+        "continent": "Oceania",
+        "capital": "Wellington",
+        "population": 5084300,
+        "currency": {
+                "officialName": "New Zealand dollar",
+                "symbol": "$",
+                "name": "NZD"
+                 },
+        "areaKM": 270467,
+        "highestPoint": {
+                        "tallestpointM": 3754,
+                        "location": "Mt. Cook"
+                        },
+        "olympicMedals":{
+                  "totalMedalcount": 137,
+                  "medals": {"gold": 53, "silver": 33, "bronze": 51}
+                  },
+        "About" : "New Zealand is an island country in the southwestern Pacific Ocean. It consists of two main landmasses—the North Island and the South Island —and over 700 smaller islands.",
+        "officialLanguage": "English",
+        "independent": true,
+        "unMember": true,
+        "flag": "https://flagcdn.com/nz.svg",
+        "borderingCountries": 0,
+        "nationalAnimal": "Kiwi",
+        "animalPhoto": "https://en.wikipedia.org/wiki/List_of_national_animals#/media/File:TeTuatahianui.jpg",
+        "googleMap": "https://goo.gl/maps/xXiDQo65dwdpw9iu8"
+    },
+  
+]
