17b95297c3fceb03f41fc83fcbddc1456b9cc5e3
rogertmalg <roger.malgueira@outlook.com>
2021-12-14 12:29:51 +0000

    some more css

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -10,7 +10,7 @@ const Game = ({playerCountry, computerCountry}) => {
             <video
              autoPlay loop muted style ={{  position: "absolute", width: "100%", left: "50%", top: "50%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%)", zIndex: "-1"}}
             >
-            <source src="https://ak.picdn.net/shutterstock/videos/1938247/preview/stock-footage-a-cartoon-spinning-earth-this-animation-is-a-seamless-loop.webm" type="video/mp4"/>
+            <source src="https://youtu.be/oONunac1ha44" type="video/mp4"/>
             </video>
         </div>
     );
