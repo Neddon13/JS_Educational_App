0ecc51fdd1d59b72a4b89006e411615dc0cad5c8
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-12-15 11:54:41 +0000

    added headings

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,15 @@ const About = ({handleStart}) => {
     return(
         <div className="about-div">
             <h2>Welcome to Countries Top Trumps</h2>
-            <button onClick={handleStart}>Start Game</button>
+            <h4>About</h4>
+            <p>
+            <h5>Introduction</h5>
+                Top Trumps is the name of a series of themed card games first introduced by the UK company Dubreq in the 1970's. Each pack is based on a particular subject, for example cars, aircraft, sports personalities, fictional heroes. Since the 1980's many different manufacturers have produced versions of this game under various names.
+            </p>
+            <h4>How to Play</h4>
+            <p>
+            </p>
+            <button className="play-button" onClick={handleStart}>Play Now!</button>
         </div>
     )
 }
