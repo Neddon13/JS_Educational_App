5a45bd4b06da9664dd126ccf76932ce0ce596caf
rogertmalg <roger.malgueira@outlook.com>
2021-12-15 15:01:16 +0000

    adds country name to computer card title

--- client/src/components/Computer.js
+++ client/src/components/Computer.js
@@ -3,7 +3,7 @@ import ComputerCard from "./ComputerCard";
 const Computer = ({computerCountry}) => {
     return (
         <div className='computer-div'>
-            <h3> Computer's card </h3>
+            <h3>Computer's card: {computerCountry.name}</h3>
             <ComputerCard computerCountry={computerCountry}/>
         </div>
     );
