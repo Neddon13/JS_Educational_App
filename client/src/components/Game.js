8046a60c707dae05e64ea4bbaceffc500dedd4b0
rogertmalg <roger.malgueira@outlook.com>
2021-12-15 14:33:09 +0000

    adds popup logic

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,14 +1,17 @@
 import React,{useState} from "react";
+import Modal from 'react-modal';
 import Player from "./Player";
 import Computer from "./Computer";
 import Score from "./Score";
 import globe from "../media/globe.mp4"
+import Popup from "./PopUp";
 
-const Game = ({playerCountry, computerCountry, handleRound}) => {
+const Game = ({playerCountry, computerCountry, handleRound, setRoundtoZero, setRoundtoOne, toggleReplay}) => {
     const [playerChoice, setPlayerChoice] = useState(null);
     const [computerValue, setComputerValue] = useState(null);
-    const [playerScore, setPlayerScore] = useState(0)
-    const [computerScore, setComputerScore] = useState(0)
+    const [playerScore, setPlayerScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const [winner, setWinner] = useState("")
 
     const handleChoice = (choice) => {
@@ -20,11 +23,43 @@ const Game = ({playerCountry, computerCountry, handleRound}) => {
         if (playerChoice > computerValue) {
             setWinner("Player")
             setPlayerScore(playerScore + 1)  
-            handleRound()     
+            handleRound()
+            setModalIsOpenToTrue()     
         } else {
             setWinner("Computer");
             setComputerScore(computerScore + 1)
             handleRound()
+            setModalIsOpenToTrue()  
+        }
+    };
+
+    const setModalIsOpenToTrue = () => {
+        if (playerScore == 8 || computerScore == 8) {
+            setModalIsOpen(true)
+        }  
+    }
+
+    const setModalIsOpenToFalse =()=>{
+        setModalIsOpen(false)
+        setRoundtoOne()
+        resetScores()
+        toggleReplay()
+    }
+
+    const resetScores = () => {
+        setPlayerScore(0)
+        setComputerScore(0)
+    }
+
+    const customStyles = {
+        content : {
+          top                   : '50%',
+          left                  : '50%',
+          right                 : 'auto',
+          bottom                : 'auto',
+          marginRight           : '-50%',
+          transform             : 'translate(-50%, -50%)',
+          backgroundColor       : '#F0AA89'      
         }
     };
 
@@ -40,6 +75,12 @@ const Game = ({playerCountry, computerCountry, handleRound}) => {
             </video>
             <Score playerScore={playerScore} computerScore={computerScore}/>
             <button className="glow-on-hover" onClick={handleClick}>Submit</button>
+
+            <Modal isOpen={modalIsOpen} style={customStyles}>
+                <button onClick={setModalIsOpenToFalse}>Play Again</button>
+                <button onClick={setRoundtoZero}>Home</button>
+                <Popup />
+            </Modal>
         </div>
     );
 };
